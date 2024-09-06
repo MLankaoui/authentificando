0903e07988b6f50c55f13cf7d484ad16ffd87718
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-06 15:30:14 +0100

    implement signup with error checking.

--- authentificando/settings.py
+++ authentificando/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILESDIRS = [
+STATICFILES_DIRS  = [
     os.path.join(BASE_DIR, 'static')
 ]
 
