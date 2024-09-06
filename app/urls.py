51587ac35747031d0cb46e478ac5e173d6f1bfa0
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-06 16:34:15 +0100

    fix bug, all good.

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from app.views import register_page, login_page
+from app.views import register_page, login_page, home_page, logout_user
 
 
 urlpatterns = [
-    path('', register_page, name='register'),
-    path('login/', login_page, name='login')
+    path('', home_page, name='home'),
+    path('register/', register_page, name='register'),
+    path('login/', login_page, name='login'),
+    path('logout/', logout_user, name='logout')
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from app.forms import createUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+def home_page(request):
+    return render(request, 'assets/home.html')
 
 def register_page(request):
     form = createUserForm()
@@ -8,6 +14,9 @@ def register_page(request):
         form = createUserForm(request.POST)
         if form.is_valid():
             form.save()
+            # getting username
+            user_name = form.cleaned_data.get('username')
+            messages.success(request, f'account created with the username : {user_name}')
             return redirect('login')  # Redirect to the login page or another page after successful registration
     context = {"form": form}
     return render(request, 'assets/signup.html', context)
@@ -15,4 +24,21 @@ def register_page(request):
 
 
 def login_page(request):
+    if request.method == "POST":
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=user_name, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'{user_name} logged in successfully')
+            return redirect('home')
+        else:
+            messages.warning(request, f'username or password incorrect')
     return render(request, 'assets/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
